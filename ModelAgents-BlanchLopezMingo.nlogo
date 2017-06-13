061000cedc6d25d4539735b5a78051384b957a58
Arnau Blanch <ArnauBlanch@users.noreply.github.com>
2017-06-13 18:54:44 +0200

    R files added

--- ModelAgents-BlanchLopezMingo.nlogo
+++ ModelAgents-BlanchLopezMingo.nlogo
@@ -1,5 +1,3 @@
-extensions [table]
-
 breed [ trains train ]
 breed [ stations station ]
 
@@ -419,7 +417,7 @@ max-capacity
 max-capacity
 0
 1000
-400.0
+350.0
 1
 1
 passengers
@@ -445,7 +443,7 @@ train-frequency-exponential
 train-frequency-exponential
 1
 15
-5.0
+4.0
 0.1
 1
 NIL
@@ -1181,21 +1179,16 @@ NetLogo 6.0.1
 @#$#@#$#@
 @#$#@#$#@
 <experiments>
-  <experiment name="experiment" repetitions="3" runMetricsEveryStep="true">
+  <experiment name="experiment" repetitions="20" runMetricsEveryStep="false">
     <setup>setup</setup>
     <go>go</go>
     <exitCondition>hour = 24 and minute = 0 and second = 0</exitCondition>
     <metric>mean-waiting-time / ticks-per-minute</metric>
     <enumeratedValueSet variable="max-capacity">
-      <value value="400"/>
-      <value value="300"/>
-      <value value="200"/>
-    </enumeratedValueSet>
-    <enumeratedValueSet variable="mean-time-between-stations">
-      <value value="1.5"/>
+      <value value="350"/>
     </enumeratedValueSet>
     <enumeratedValueSet variable="train-frequency-exponential">
-      <value value="6.7"/>
+      <value value="4"/>
     </enumeratedValueSet>
   </experiment>
 </experiments>
---
+++ R/Càlcul nombre de rèpliques.r
@@ -0,0 +1,44 @@
+# install.packages("rstudioapi") # run this if it's your first time using it to install
+library(rstudioapi) # load it
+# the following line is for getting the path of your current open file
+current_path <- getActiveDocumentContext()$path 
+# The next line set the working directory to the relevant one:
+setwd(dirname(current_path ))
+
+# Importem les dades de la prova pilot
+library(readxl)
+ProvaPilot <- read_excel("ProvaPilot-table.xlsx", 
+col_types = c("blank", "blank", "blank", "blank", "numeric"))
+ProvaPilot <- ProvaPilot$`mean-waiting-time / ticks-per-minute`
+
+# Resum de les dades
+summary(ProvaPilot)
+
+sink("Resultat nombre de rèpliques.txt")
+# Nombre de rèpliques
+n <- length(ProvaPilot)
+cat("Nombre de rèpliques prova pilot =", n, "\n")
+# Mitjana
+X <- mean(ProvaPilot)
+cat("Mitjana prova pilot =", X, "\n")
+# Desviació
+S <- sd(ProvaPilot)
+cat("Desviació prova pilot =", n, "\n\n")
+
+# Semi-amplada de l'interval de confiança de la prova pilot
+h <- qt(0.975, n-1) * S/sqrt(n)
+cat("Semi-amplada de l'interval de confiança de la prova pilot =", h, "\n")
+
+# Interval de confiança del 95%
+IC <- c(X - h, X + h)
+cat("Interval de confiança (α = 0.05) = [", IC[1], ",", IC[2], "]", "\n")
+
+# Semi-interval de confiança desitjat (95% de confiança)
+H <- 0.05 * X
+cat("Semi-amplada de l'interval de confiança desitjat (95% de confiança) =", H, "\n")
+
+# Nombre de rèpliques necessàries
+N <- n * (h/H)^2
+cat("\nNombre de rèpliques necessàries =", N, "->", ceiling(N), "\n")
+
+sink()
